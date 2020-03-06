da96549addefb1dc415ecd4859a5828119fb230c
Dai Ibrahim <33212069+Dai-Ibrahim@users.noreply.github.com>
2020-03-06 01:34:42 -0700

    Add files via upload

---
+++ Script/EventPublisher.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+public class EventPublisher : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown("s"))
+        {
+            EventBus.TriggerEvent("Shoot");
+        }
+
+        if (Input.GetKeyDown("l"))
+        {
+            EventBus.TriggerEvent("Launch");
+        }
+		if (Input.GetKeyDown("c"))
+        {
+            EventBus.TriggerEvent("Scream");
+        }
+		if (Input.GetKeyDown("h"))
+        {
+            EventBus.TriggerEvent("Honk");
+        }
+
+		if (Input.GetKeyDown("b"))
+        {
+            EventBus.TriggerEvent("Boom");
+        }
+
+
+    }
+}
